892a8a3f1200a133c14f6e77b4f0d7f5fd66b396
AnnaPudlo <a.fomenko91@gmail.com>
2020-01-19 22:33:51 +0200

    add the report scatterplot using d3, but without styles

--- app/js/app.js
+++ app/js/app.js
@@ -31,6 +31,8 @@ $(document).ready(function () {
 });
 
 //======= start using d3 =======//
+
+//======= start sales chart =======//
 let dataset = [
   { label: 'Websites', count: 12 },
   { label: 'Logo', count: 13 },
@@ -47,7 +49,7 @@ let legendRectSize = 18;
 let legendSpacing = 4;
 
 let color = d3.scaleOrdinal().domain(dataset.length)
-.range(["#4b74e0", "#4164c2", "#3755a4", "#25396e", "#5584ff"])
+  .range(["#4b74e0", "#4164c2", "#3755a4", "#25396e", "#5584ff"])
 
 let svg = d3.select('#ba-sales-chart')
   .append('svg')
@@ -87,9 +89,9 @@ let legend = svg.selectAll('.legend')
   });
 
 legend.append('circle')
-  .attr('cx', legendRectSize/2)
-  .attr('cy', legendRectSize/2)
-  .attr('r', legendRectSize/2)
+  .attr('cx', legendRectSize / 2)
+  .attr('cy', legendRectSize / 2)
+  .attr('r', legendRectSize / 2)
   .style('fill', color)
   .style('stroke', color);
 
@@ -102,3 +104,84 @@ svg.append('text')
   .attr('x', 0)
   .attr('y', 0)
   .text('1,560 sales')
+//======= end sales chart =======//
+
+//======= start report chart =======//
+let margin = { top: 10, right: 30, bottom: 30, left: 60 };
+let reportWidth = 460 - margin.left - margin.right;
+let reportHeight = 200 - margin.top - margin.bottom;
+
+let reportSvg = d3.select("#ba-report-chart")
+  .append("svg")
+  .attr("width", reportWidth + margin.left + margin.right)
+  .attr("height", reportHeight + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
+
+let reportData = [
+  { date: "2019-01-01", value: 300 },
+  { date: "2019-02-01", value: 370 },
+  { date: "2019-03-01", value: 250 },
+  { date: "2019-04-01", value: 500 },
+  { date: "2019-05-01", value: 350 },
+  { date: "2019-06-01", value: 740 },
+  { date: "2019-07-01", value: 730 },
+  { date: "2019-08-01", value: 400 },
+  { date: "2019-09-01", value: 550 },
+  { date: "2019-10-01", value: 300 },
+  { date: "2019-11-01", value: 380 },
+  { date: "2019-12-01", value: 290 },
+];
+
+function parseDate(d) {
+  return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value }
+}
+
+for (let i = 0; i < reportData.length; i++)
+  reportData[i] = parseDate(reportData[i]);
+console.log(reportData);
+
+let reportX = d3.scaleTime()
+  .domain(d3.extent(reportData, function (d) { return d.date; }))
+  .range([0, reportWidth]);
+reportSvg.append("g")
+  .attr("transform", "translate(0," + reportHeight + ")")
+  .call(d3.axisBottom(reportX).ticks(12).tickSize(0).tickFormat(""));
+
+let gridlinesV = d3.axisBottom()
+  .tickFormat("")
+  .tickSize(reportHeight)
+  .scale(reportX);
+
+reportSvg.append("g")
+  .attr("class", "gridV")
+  .call(gridlinesV);
+
+let reportY = d3.scaleLinear()
+  .domain([100, 790])
+  .range([reportHeight, 0]);
+reportSvg.append("g").call(d3.axisLeft(reportY).ticks(5).tickSize(0));
+
+let gridlinesH = d3.axisRight()
+  .ticks(5)
+  .tickFormat("")
+  .tickSize(reportWidth)
+  .scale(reportY);
+
+reportSvg.append("g")
+  .attr("class", "gridH")
+  .call(gridlinesH);
+
+
+reportSvg.append("path")
+  .datum(reportData)
+  .attr("fill", "none")
+  .attr("stroke", "blue")
+  .attr("stroke-width", 1.5)
+  .attr("d", d3.line()
+    .curve(d3.curveBasis)
+    .x(function (d) { return reportX(d.date) })
+    .y(function (d) { return reportY(d.value) }))
+
+//======= end report chart =======//
--- build/js/app.js
+++ build/js/app.js
@@ -28,6 +28,7 @@ $(document).ready(function () {
     }
   });
 }); //======= start using d3 =======//
+//======= start sales chart =======//
 
 var dataset = [{
   label: 'Websites',
@@ -71,4 +72,80 @@ legend.append('circle').attr('cx', legendRectSize / 2).attr('cy', legendRectSize
 legend.append('text').attr('x', legendRectSize + legendSpacing).attr('y', legendRectSize - legendSpacing).text(function (d) {
   return d;
 });
-svg.append('text').attr('x', 0).attr('y', 0).text('1,560 sales');
+svg.append('text').attr('x', 0).attr('y', 0).text('1,560 sales'); //======= end sales chart =======//
+//======= start report chart =======//
+
+var margin = {
+  top: 10,
+  right: 30,
+  bottom: 30,
+  left: 60
+};
+var reportWidth = 460 - margin.left - margin.right;
+var reportHeight = 200 - margin.top - margin.bottom;
+var reportSvg = d3.select("#ba-report-chart").append("svg").attr("width", reportWidth + margin.left + margin.right).attr("height", reportHeight + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var reportData = [{
+  date: "2019-01-01",
+  value: 300
+}, {
+  date: "2019-02-01",
+  value: 370
+}, {
+  date: "2019-03-01",
+  value: 250
+}, {
+  date: "2019-04-01",
+  value: 500
+}, {
+  date: "2019-05-01",
+  value: 350
+}, {
+  date: "2019-06-01",
+  value: 740
+}, {
+  date: "2019-07-01",
+  value: 730
+}, {
+  date: "2019-08-01",
+  value: 400
+}, {
+  date: "2019-09-01",
+  value: 550
+}, {
+  date: "2019-10-01",
+  value: 300
+}, {
+  date: "2019-11-01",
+  value: 380
+}, {
+  date: "2019-12-01",
+  value: 290
+}];
+
+function parseDate(d) {
+  return {
+    date: d3.timeParse("%Y-%m-%d")(d.date),
+    value: d.value
+  };
+}
+
+for (var i = 0; i < reportData.length; i++) {
+  reportData[i] = parseDate(reportData[i]);
+}
+
+console.log(reportData);
+var reportX = d3.scaleTime().domain(d3.extent(reportData, function (d) {
+  return d.date;
+})).range([0, reportWidth]);
+reportSvg.append("g").attr("transform", "translate(0," + reportHeight + ")").call(d3.axisBottom(reportX).ticks(12).tickSize(0).tickFormat(""));
+var gridlinesV = d3.axisBottom().tickFormat("").tickSize(reportHeight).scale(reportX);
+reportSvg.append("g").attr("class", "gridV").call(gridlinesV);
+var reportY = d3.scaleLinear().domain([100, 790]).range([reportHeight, 0]);
+reportSvg.append("g").call(d3.axisLeft(reportY).ticks(5).tickSize(0));
+var gridlinesH = d3.axisRight().ticks(5).tickFormat("").tickSize(reportWidth).scale(reportY);
+reportSvg.append("g").attr("class", "gridH").call(gridlinesH);
+reportSvg.append("path").datum(reportData).attr("fill", "none").attr("stroke", "blue").attr("stroke-width", 1.5).attr("d", d3.line().curve(d3.curveBasis).x(function (d) {
+  return reportX(d.date);
+}).y(function (d) {
+  return reportY(d.value);
+})); //======= end report chart =======//
